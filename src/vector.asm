4ff5044505ff2ad9126e23f736c2822b4e8b3494
Brian Steffens <briansteffens@gmail.com>
2016-06-09 17:41:51 +0000

    Add vector_resize and vector_get

--- src/vector.asm
+++ src/vector.asm
@@ -1,6 +1,7 @@
 %include "common.asm"
 
 extern allocate
+extern reallocate
 extern deallocate
 
 %define HEADER_SIZE 24
@@ -77,6 +78,116 @@ vector_len:
 
 ; Return the count
     mov rax, [rbx + HEADER_OFFSET_COUNT]
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
+
+;   Function vector_resize
+;       Change the size of the vector, reallocating if necessary
+;
+;   Stack arguments:
+;       VECTOR_ADDR - The address of the vector to resize
+;       NEW_COUNT   - The new element count
+;
+;   Return arguments:
+;       rax - The new address of the vector (if it changed)
+;             If < 0, there was an error
+
+global vector_resize:function
+    %define RESIZE_VECTOR_ADDR 24
+    %define RESIZE_NEW_COUNT 16
+vector_resize:
+    push rbp
+    mov rbp, rsp
+
+; Get the start of the vector header
+    mov rbx, [rbp + RESIZE_VECTOR_ADDR]
+    sub rbx, HEADER_SIZE
+
+; If the new size will not grow the array, this is a no-op
+    mov rcx, [rbp + RESIZE_NEW_COUNT]
+    cmp rcx, [rbx + HEADER_OFFSET_COUNT]
+    jle vector_resize_no_op
+
+; If enough room has been previously allocated, we don't need to reallocate
+    cmp rcx, [rbx + HEADER_OFFSET_ALLOCATED]
+    jle vector_resize_no_reallocate
+
+; Pre-allocate twice what is requested
+; TODO: consider other allocation strategies
+    imul rcx, 2
+    push rcx
+
+; Calculate new size in bytes
+    mov rcx, rdx
+    imul rcx, [rbx + HEADER_OFFSET_ELEMENT_SIZE]
+    add rcx, HEADER_SIZE
+
+; Allocate more space
+    push rbx
+    push rcx
+    call reallocate
+    add rsp, 16
+    cmp rax, 0
+    jl vector_resize_ret
+    mov rbx, rax
+
+; Save new allocated count to vector header
+    pop rcx
+    mov [rbx + HEADER_OFFSET_ALLOCATED], rcx
+
+vector_resize_no_reallocate:
+
+; Update count in header
+    mov rcx, [rbp + RESIZE_NEW_COUNT]
+    mov [rbx + HEADER_OFFSET_COUNT], rcx
+
+; Return first element of vector
+    add rbx, HEADER_SIZE
+    mov rax, rbx
+
+    jmp vector_resize_ret
+
+vector_resize_no_op:
+    mov rax, [rbp + RESIZE_VECTOR_ADDR]
+    jmp vector_resize_ret
+
+vector_resize_ret:
+    mov rsp, rbp
+    pop rbp
+    ret
+
+
+;   Function vector_get
+;       Get the address of an element in a vector by index
+;
+;   Stack arguments:
+;       VECTOR_ADDR - The address of the vector
+;       INDEX       - The 0-based index of the element
+;
+;   Return values:
+;       rax - If > 0, the address of the element. Otherwise, error.
+
+global vector_get:function
+    %define GET_VECTOR_ADDR 24
+    %define GET_INDEX 16
+vector_get:
+    push rbp
+    mov rbp, rsp
+
+; Get vector element size
+    mov rbx, [rbp + GET_VECTOR_ADDR]
+    sub rbx, HEADER_SIZE
+    mov rax, [rbx + HEADER_OFFSET_ELEMENT_SIZE]
+
+; Multiply by index to get starting offset from first element
+    imul rax, [rbp + GET_INDEX]
+
+; Add vector header start and vector header size to get actual address
+    add rax, rbx
+    add rax, HEADER_SIZE
 
     mov rsp, rbp
     pop rbp
--- tests/test_vector.asm
+++ tests/test_vector.asm
@@ -2,11 +2,16 @@
 
 extern vector_new
 extern vector_len
+extern vector_resize
+extern vector_get
 extern vector_free
+
+extern allocate
 
 section .bss
 
     vec resq 1
+    temp resq 1
 
 section .text
 
@@ -26,6 +31,36 @@ _start:
     add rsp, 8
     cmp rax, 0
     jl err
+
+    push qword [vec]
+    push 1
+    call vector_resize
+    add rsp, 16
+    cmp rax, 0
+    jl err
+    mov [vec], rax
+
+    push 3
+    call allocate
+    cmp rax, 0
+    jl err
+    mov [temp], rax
+
+    mov rcx, 0
+    mov byte [rax + rcx], 'H'
+    inc rcx
+    mov byte [rax + rcx], 'i'
+    inc rcx
+    mov byte [rax + rcx], 0
+
+    push qword [vec]
+    push 0
+    call vector_get
+    add rsp, 16
+    cmp rax, 0
+    jl err
+    mov rbx, temp
+    mov qword [rax], rbx
 
     push qword [vec]
     call vector_free
